181f8db2f488249adf9d11650c16e4b02dcafe40
Anderson Duarte <andersonduarte997@gmail.com>
2023-01-21 19:40:57 -0300

    Versão 1.2 - Verificação de usuários ativos implementada

--- js/script.js
+++ js/script.js
@@ -12,20 +12,34 @@ function inicio(){
 }
 inicio();
 
-function confirmacaoDePermanencia(){
-    const requisicao = axios.post('https://mock-api.driven.com.br/api/v6/uol/status', nome);
-}
-
 function nomeAceito(requisicao){
-    console.log("Status code: " + requisicao.status);
-    /*setInterval(confirmacaoDePermanencia, 5000);*/
+    setInterval(()=>{
+        axios.post('https://mock-api.driven.com.br/api/v6/uol/status', nome);
+    }, 5000);
+    /*Este setInterval executa uma função que verifica se o usuário permanece logado.*/
+    setInterval(()=>{
+        const listaUsuarios = axios.get('https://mock-api.driven.com.br/api/v6/uol/participants');
+        listaUsuarios.then((resposta)=>{
+            const usuarioAtivos = document.querySelector('.usuariosOnline');
+            usuarioAtivos.innerHTML = "";
+            for(let i=0; i<resposta.data.length; i++){
+                usuarioAtivos.innerHTML += `<div class="opcoes">
+                <ion-icon name="person-circle"></ion-icon>
+                <h2>${resposta.data[i].name}</h2>
+            </div>`;
+            }
+        });
+    }, 1000);
+    /*Este setInterval está executando uma função arrow que verifica a lista de usuários online
+    listaUsuarios está recebendo a promise e o then usado para executar essa requisição está com
+    uma função arrow também, que vai inserir na página os usuários online*/
+    
     const promessa = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
     promessa.then(mensagensRecebidas);
     promessa.catch(mensagensNaoRecebidas);
 
 }
 function nomeRecusado(requisicao){
-    console.log("Status code: " + requisicao.response.status);
     alert("Esse nome já está em uso.");
     inicio();
 }
@@ -33,15 +47,41 @@ function nomeRecusado(requisicao){
 function inserirAtualizarMensagens(){
     const mainMensagens = document.querySelector('main');
     for(let i=0; i<mensagens.length; i++){
-        mainMensagens.innerHTML += `<div>
-        <span>${mensagens[i].time}</span>
-        <span>${mensagens[i].from}</span>
-        <span>${mensagens[i].text}</span>
-        </div>`;
+       
+        if(mensagens[i].type==="status"){
+            mainMensagens.innerHTML += `<div class="mensagensStatus">
+            <span>
+            <span class="tempo">(${mensagens[i].time})</span>
+            <span class="nomeUsuario">${mensagens[i].from}</span>
+            <span class="mensagemTexto">${mensagens[i].text}</span>
+            </span>
+            </div>`;
+        }else if(mensagens[i].type==="message"){
+            mainMensagens.innerHTML += `<div class="mensagensNaoPrivadas">
+            <span>
+            <span class="tempo">(${mensagens[i].time})</span>
+            <span class="nomeUsuario">${mensagens[i].from}</span>
+            <span> para </span>
+            <span class="nomeUsuario">${mensagens[i].to}:</span>
+            <span class="mensagemTexto">${mensagens[i].text}</span>
+            </span>
+            </div>`;
+        }else if(mensagens[i].type==="private_message"){
+            mainMensagens.innerHTML += `<div class="mensagensPrivadas">
+            <span>
+            <span class="tempo">(${mensagens[i].time})</span>
+            <span class="nomeUsuario">${mensagens[i].from}</span>
+            <span> reservadamente para </span>
+            <span class="nomeUsuario">${mensagens[i].to}:</span>
+            <span class="mensagemTexto">${mensagens[i].text}</span>
+            </span>
+            </div>`;
+        }
+        
     }
 }
 function mensagensRecebidas(resposta){
-    alert("Dados recebidos!");
+    console.log(resposta.data);
     for(let i=0; i<resposta.data.length; i++){
         mensagens.push(resposta.data[i]);
     }
@@ -49,7 +89,6 @@ function mensagensRecebidas(resposta){
 }
 function mensagensNaoRecebidas(resposta){
     alert("Não foi possível receber os dados do servidor.");
-    console.log(resposta);
 }
 
 
